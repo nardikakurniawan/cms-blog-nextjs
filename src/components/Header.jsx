ab1e266aadd36cbf2f637753c78724b91ca1fe5f
Nardika Kurniawan <nardika.kurniawan@lumoshive.com>
2022-12-01 11:52:20 +0700

    fix erorr vercel part 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white hover:text-gray-300">
-              D'dev Blog
+              D&apos;dev Blog
             </span>
           </Link>
         </div>
